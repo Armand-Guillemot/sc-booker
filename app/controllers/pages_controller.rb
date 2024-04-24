b4cc290ae9816ac76474d28bad050154ff281194
Armand Guillemot <armand.guillemot.56@gmail.com>
2024-04-24 17:27:58 +0200

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   before_action :verify_admin, only: [:admin]
   def home
+    if current_user.admin? && params[:user_id].present? && User.exists?(params[:user_id])
+      @selected_user = User.find(params[:user_id])
+    else
+      @selected_user = current_user
+    end
   end
 
   def admin
